45bbb91f63aae754038fcf6c7f6af3db6776aba8
Raúl <raulguzmanguerrero@outlook.com>
2020-03-17 12:35:17 -0600

    Funcionalidad del botón ver más

---
+++ js/glosario.js
@@ -0,0 +1,7 @@
+// Movimiento entre templates, quitar el folder glosario/templates cuando esté en producción :)
+
+$('#definicionCompleta').click(function (e) { 
+    
+    window.location.href = '/glosario/templates/palabra.html';
+    
+});
