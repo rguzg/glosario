72a7856ec578f7e3d77e933207d635f544da41ce
Raúl <raulguzmanguerrero@outlook.com>
2020-03-17 12:18:44 -0600

    Modal de reportar palabra

--- js/modales.js
+++ js/modales.js
@@ -3,3 +3,9 @@ $('#relacionarPalabra').click(function (e) {
     $('#modalRelacionarPalabra').modal('show');
 
 });
+
+$('#reportarPalabra').click(function (e) { 
+    
+    $('#modalReportarPalabra').modal('show');
+
+});
