8dcfbbdee3b57ebf4223c7838cf54ec8aae7714a
Raúl <raulguzmanguerrero@outlook.com>
2020-03-30 18:51:36 -0600

    Perfil y cambiar contraseña

--- js/modales.js
+++ js/modales.js
@@ -9,3 +9,16 @@ $('#reportarPalabra').click(function (e) {
     $('#modalReportarPalabra').modal('show');
 
 });
+
+$('#editarInformacion').click(function (e) { 
+    
+    $('#modalEditarInformacion').modal('show');
+
+});
+
+$('#cambiarContraseña').click(function (e) { 
+    
+    $('#modalCambiarContraseña').modal('show');
+
+});
+
