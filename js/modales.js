1bff2b5b5556575ea5121da782dc8cabb2152f1e
Raúl <raulguzmanguerrero@outlook.com>
2020-03-30 20:21:07 -0600

    Modales compatibles con react

--- js/modales.js
+++ js/modales.js
@@ -34,3 +34,95 @@ $('#cambiarContraseña').click(function (e) {
 
 });
 
+// Modales React
+function reportarPalabraOpenModal() {
+    // Abre el modal a través de la variable modalReportarPalabra
+    // en el archivo modales.js de public
+    window.window.modalReportarPalabra()
+}
+
+function relacionarPalabraOpenModal(){
+    // Abre el modal a través de la variable modalRelacionarPalabra
+    // en el archivo modales.js de public
+    window.modalRelacionarPalabra() 
+}
+
+var modalRelacionarPalabra = ()=>{
+    $(function(){
+        $('#modalRelacionarPalabra').modal('show');
+    })
+}
+
+var modalReportarPalabra = ()=>{
+    $(function(){
+         $('#modalReportarPalabra').modal('show');
+    })
+}
+
+
+// Inicio de nuevas funciones
+function reportarImagenOpenModal(){
+    // Abre el modal a través de la variable modalReportarImagen
+    // en el archivo modales.js de public
+    window.modalReportarImagen()
+
+}
+
+function sugerirImagenOpenModal(){
+    // Abre el modal a través de la variable modalSugerirImagen
+    // en el archivo modales.js de public
+    window.modalSugerirImagen()
+
+}
+
+function editarInfomacionOpenModal(){
+    // Abre el modal a través de la variable modalEditarInformacion
+    // en el archivo modales.js de public
+    window.modalEditarInformacion()
+
+}
+
+function cambiarContraseñaOpenModal(){
+    // Abre el modal a través de la variable modalCambiarContraseña
+    // en el archivo modales.js de public
+    window.modalCambiarContraseña()
+
+}
+
+function cerrarCualquierModal(modal_id){
+
+    window.cerrarModal(modal_id);
+
+}
+
+var modalReportarImagen = () => {
+    $(function (e) { 
+        $('#modalReportarImagen').modal('show');
+    })
+};
+
+var modalSugerirImagen = () => {
+    $(function (e) { 
+        $('#modalSugerirImagen').modal('show');
+    })
+};
+
+var modalEditarInformacion = () => {
+    $(function (e) { 
+        $('#modalEditarInformacion').modal('show');
+    })
+};
+
+var modalCambiarContraseña = () => {
+    $(function (e) { 
+        $('#modalCambiarContraseña').modal('show');
+    })
+};
+
+var cerrarModal = (modal_id) => {
+
+    $(function (e) { 
+        $(`#${modal_id}`).modal('hide');
+    })
+
+}
